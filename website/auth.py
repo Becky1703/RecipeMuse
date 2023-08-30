0d0edc0c64e13198a0229d5e7bc8e65c16936d12
Becky1703 <Becky1703>
2023-08-31 00:52:40 +0100

    corrected errors in html file

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from website.models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -37,13 +37,16 @@ def login():
         
         user = User.query.filter_by(username=username).first()
         
-        if user and check_password_hash(user.password_hash, password):
-            login_user(user)
-            return redirect(url_for('index'))
+        if user:
+            if check_password_hash(user.password_hash, password):
+                login_user(user, remember=True)
+                flash('Logged in successfully!', 'success')
+                return redirect(url_for('views.index'))
+            else:
+             flash('Invalid username or password', 'error')
         else:
             flash('Invalid username or password', 'error')
-    
-    return render_template('login.html')
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
 @login_required
