2c59e03cea862caac1fe18b8ac5f83710e04acaf
Becky1703 <oluwabukunmia@gmail.com>
2023-09-11 12:41:44 +0000

    changed port

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app import create_app
-
 app = create_app()
 
 if __name__ == '__main__':
-     #app = create_app()
-     app.run(host='0.0.0.0', port=5007)
+     app.run(host='0.0.0.0', port=5010)
 
